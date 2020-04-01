66feecd8d9c9b3af82e0631d72f972a0f41a4419
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-01 15:43:17 -0700

    start HTML, CSS, and create getRandomThrows.js

---
+++ get-random-throw.js
@@ -0,0 +1,15 @@
+export default getRandomThrow;
+
+function getRandomThrow(){
+    const computerThrow = Math.floor(Math.random() * (3 - 1)) + 1;
+    if (computerThrow === 1){
+        return 'rock';
+    }
+    if (computerThrow === 2){
+        return 'paper';
+    }
+    if (computerThrow === 3){
+        return 'scissors';
+    }
+}
+
